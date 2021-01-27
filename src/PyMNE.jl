09c267bc2c3571b37f413034cee0fea40c54e2f2
Alex Arslan <ararslan@comcast.net>
2021-01-27 10:01:27 -0800

    Don't eval into the module during precompilation

--- src/PyMNE.jl
+++ src/PyMNE.jl
@@ -28,11 +28,13 @@ function __init__()
     # all of this is __init__() so that it plays nice with precompilation
     # see https://github.com/JuliaPy/PyCall.jl/#using-pycall-from-julia-modules
     copy!(mne, pyimport("mne"))
-    # delegate everything else to mne
-    for pn in propertynames(mne)
-        isdefined(@__MODULE__, pn) && continue
-        prop = getproperty(mne, pn)
-        @eval $pn = $prop
+    if ccall(:jl_generating_output, Cint, ()) == 0
+        # delegate everything else to mne
+        for pn in propertynames(mne)
+            isdefined(@__MODULE__, pn) && continue
+            prop = getproperty(mne, pn)
+            @eval $pn = $prop
+        end
     end
     return nothing
 end
