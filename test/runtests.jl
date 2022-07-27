4799533cf06c0dd8bacc33a36c8bb82694a7ae45
Phillip Alday <phillip.alday@beacon.bio>
2022-07-27 13:00:57 -0500

    convert package to PythonCall instead of PyCall

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,25 @@
 using PyMNE
-using PyCall
+using Random
 using Test
 
-using PyCall: PyError
-
-@testset "sklearn" begin
-    PyMNE.install_sklearn()
-    # requires sklearn
-    @test PyMNE.preprocessing.ICA(method="fastica") isa PyObject
-end
 
 @testset "create_info and get_info" begin
-    dat = zeros(1, 100)
-    naive_info = PyMNE.mne.create_info([:a], 100)
-    wrapped_info = PyMNE.create_info([:a], 100)
-    raw = PyMNE.io.RawArray(dat, wrapped_info)
-    @test raw.get_data() == dat
-    @test get_info(raw) isa PyObject
-    @test raw.info isa Dict
-    @test_throws PyError PyMNE.io.RawArray(dat, naive_info)
+    dat = rand(1, 100)
+    info = PyMNE.create_info(pylist(["a"]), 100)
+    @test info isa Py
+    @test pyconvert(String, only(info.ch_names)) == "a"
+    # XXX why all the pyconvert? well comparisons of python objects give you
+    # a python boolean, so we need to explicitly convert for @OTDE
+    # this is fine because it shows usage and tests out a bit more code
+    @test pyconvert(Bool, info.ch_names == info["ch_names"])
+    # if this ever works after a compat bump then we know we need to change things
+    @test_broken info.ch_names == info["ch_names"]
+    @test pyconvert(Bool, info["sfreq"] == 100)
+    @test pyconvert(Number, info["sfreq"]) == 100
+    raw = PyMNE.io.RawArray(dat, info)
+    @test pyconvert(Number, raw.n_times) == 100
+    @test pyconvert(Number, raw.n_times) isa Int
+    @test pyconvert(Number, raw.info["sfreq"]) == 100
+    # we want elementwise precise equality
+    @test all(pyconvert(AbstractArray, raw.get_data()) .== dat)
 end
